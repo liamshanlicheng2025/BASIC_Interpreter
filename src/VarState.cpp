c31e9c151b9dc8605a0b5afd23443d673a0f7707
单立铖（Licheng Shan) <liamshanlicheng2025@sjtu.edu.cn>
2025-12-04 20:08:00 +0800

    Update VarState.cpp for SCOPE

--- src/VarState.cpp
+++ src/VarState.cpp
@@ -4,18 +4,56 @@
 
 #include "../include/Error.hpp"
 
+VarState::VarState() {
+    // 初始化全局作用域
+    scopes_.push(std::unordered_map<std::string, int>());
+}
+
 void VarState::setValue(const std::string& name, int value) {
-    values_[name] = value;
+    // 在当前作用域设置变量值
+    scopes_.top()[name] = value;
 }
 
 int VarState::getValue(const std::string& name) const {
-    auto it = values_.find(name);
-    if (it == values_.end()) {
-        throw BasicError("VARIABLE NOT DEFINED");
+    // 从当前作用域开始查找变量
+    std::stack<std::unordered_map<std::string, int>> temp = scopes_;
+    while (!temp.empty()) {
+        auto it = temp.top().find(name);
+        if (it != temp.top().end()) {
+            return it->second;
+        }
+        temp.pop();
     }
-    return it->second;
+    throw BasicError("VARIABLE NOT DEFINED");
 }
 
-void VarState::clear() { values_.clear(); }
+void VarState::clear() {
+    // 清空所有作用域
+    while (!scopes_.empty()) {
+        scopes_.pop();
+    }
+    scopes_.push(std::unordered_map<std::string, int>());
+}
+
+void VarState::enterScope() {
+    scopes_.push(std::unordered_map<std::string, int>());
+}
+
+void VarState::exitScope() {
+    checkScopeUnderflow();
+    if (scopes_.size() > 1) {
+        scopes_.pop();
+    }
+}
+
+int VarState::getScopeDepth() const {
+    return scopes_.size() - 1;  // 深度从0开始
+}
+
+void VarState::checkScopeUnderflow() const {
+    if (scopes_.size() <= 1) {
+        throw BasicError("SCOPE UNDERFLOW");
+    }
+}
 // Created by Lenovo on 2025/11/19.
 //
